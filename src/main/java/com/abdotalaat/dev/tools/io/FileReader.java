5f45af4caaea5eb67c6d5fa47f27876a294abf4d
abdotalaat <abdotalaatfcih@gmail.com>
2019-01-29 22:48:54 +0400

    add logger ==> slf4j

--- src/main/java/com/abdotalaat/dev/tools/io/FileReader.java
+++ src/main/java/com/abdotalaat/dev/tools/io/FileReader.java
@@ -4,6 +4,9 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
@@ -12,10 +15,13 @@ public class FileReader {
 
 	private static Gson gson = new Gson();
 
+	final static Logger logger = LoggerFactory.getLogger(FileReader.class);
+
 	private FileReader() {
 	}
 
 	public static String readFileAsString(String filePath) throws IOException {
+		logger.info("Start read file in the following path [{}]", filePath);
 		String content = new String(Files.readAllBytes(Paths.get(filePath)));
 		return content;
 	}
